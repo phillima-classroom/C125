e5aeafb351cd85530201b986cd824f3b91cb1410
phillima <phyllipe_slf@yahoo.com.br>
2020-08-05 20:12:58 -0300

    Delete old Collections

--- Aula15Colecao/Exercicio2/br/inatel/cdg/inimigo/BlackKnight.java
+++
@@ -1,9 +0,0 @@
-package br.inatel.cdg.inimigo;
-
-public class BlackKnight extends Inimigo {
-
-	public BlackKnight(String nome, int vida) {
-		super(nome, vida);
-	}
-
-}
--- Aula15Colecao/Exercicio2/br/inatel/cdg/inimigo/FireKnight.java
+++
@@ -1,9 +0,0 @@
-package br.inatel.cdg.inimigo;
-
-public class FireKnight extends Inimigo {
-
-	public FireKnight(String nome, int vida) {
-		super(nome, vida);
-	}
-
-}
