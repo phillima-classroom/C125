8a0eb989d35eb3daa9d63d76b5013fe2ab3418df
phillima <phyllipe_slf@yahoo.com.br>
2020-11-21 18:57:34 -0300

    Aula-16-JavaSwing PT1

---
+++ Aula-16-JavaGUI-PT1/src/br/inatel/cdg/JFrameMain.java
@@ -0,0 +1,46 @@
+package br.inatel.cdg;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import br.inatel.cdg.panel.PanelCadastro;
+
+public class JFrameMain extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrameMain frame = new JFrameMain();
+					frame.add(new PanelCadastro());
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrameMain() {
+		setTitle("Aplicação para Cadastro");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
